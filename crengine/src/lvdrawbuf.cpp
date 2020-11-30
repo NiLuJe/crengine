0b52f1a60c03de171dd2e8e76caa09d25260d9a4
NiLuJe <ninuje@gmail.com>
2020-11-30 20:53:03 +0100

    Pepper some const in there

--- crengine/src/lvdrawbuf.cpp
+++ crengine/src/lvdrawbuf.cpp
@@ -1892,13 +1892,13 @@ void LVColorDrawBuf::Draw( int x, int y, const lUInt8 * bitmap, int width, int h
             size_t px_count == width;
             while (px_count--)
             {
-                lUInt32 opaque = ((*(src++))>>4)&0x0F;
+                const lUInt32 opaque = ((*(src++))>>4)&0x0F;
                 if ( opaque>=0xF )
                     *dst++ = bmpcl16;
                 else if ( opaque>0 ) {
-                    lUInt32 alpha = 0xF-opaque;
-                    lUInt16 cl1 = (lUInt16)(((alpha*((*dst)&0xF81F) + opaque*(bmpcl16&0xF81F))>>4) & 0xF81F);
-                    lUInt16 cl2 = (lUInt16)(((alpha*((*dst)&0x07E0) + opaque*(bmpcl16&0x07E0))>>4) & 0x07E0);
+                    const lUInt32 alpha = 0xF-opaque;
+                    const lUInt16 cl1 = (lUInt16)(((alpha*((*dst)&0xF81F) + opaque*(bmpcl16&0xF81F))>>4) & 0xF81F);
+                    const lUInt16 cl2 = (lUInt16)(((alpha*((*dst)&0x07E0) + opaque*(bmpcl16&0x07E0))>>4) & 0x07E0);
                     *dst++ = cl1 | cl2;
                 }
             }
@@ -1916,13 +1916,13 @@ void LVColorDrawBuf::Draw( int x, int y, const lUInt8 * bitmap, int width, int h
             size_t px_count == width;
             while (px_count--)
             {
-                lUInt32 opaque = ((*(src++))>>1)&0x7F;
+                const lUInt32 opaque = ((*(src++))>>1)&0x7F;
                 if ( opaque>=0x78 )
                     *dst++ = bmpcl32;
                 else if ( opaque>0 ) {
-                    lUInt32 alpha = 0x7F-opaque;
-                    lUInt32 cl1 = ((alpha*((*dst)&0xFF00FF) + opaque*(bmpcl32&0xFF00FF))>>7) & 0xFF00FF;
-                    lUInt32 cl2 = ((alpha*((*dst)&0x00FF00) + opaque*(bmpcl32&0x00FF00))>>7) & 0x00FF00;
+                    const lUInt32 alpha = 0x7F-opaque;
+                    const lUInt32 cl1 = ((alpha*((*dst)&0xFF00FF) + opaque*(bmpcl32&0xFF00FF))>>7) & 0xFF00FF;
+                    const lUInt32 cl2 = ((alpha*((*dst)&0x00FF00) + opaque*(bmpcl32&0x00FF00))>>7) & 0x00FF00;
                     *dst++ = cl1 | cl2;
                 }
             }
